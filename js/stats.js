851e964d0a60de62db06f240f619e8475c6df674
Alexandra Larina <aa.larina.ff@gmail.com>
2017-11-21 18:42:12 +0300

    Добавлены функции отрисовки столбца гистограммы, переноса текста и рандомной прозрачности столбца.

--- js/stats.js
+++ js/stats.js
@@ -3,8 +3,8 @@
 window.renderStatistics = function (ctx, names, times) {
   /**
    * Параметры облака
-   * @const
-   * @type {number}
+   * @readonly
+   * @enum {number}
    */
   var CloudParams = {
     WIDTH: 420,
@@ -15,27 +15,31 @@ window.renderStatistics = function (ctx, names, times) {
   };
   /**
    * Параметры текста
-   * @const
-   * @type {number}
+   * @readonly
+   * @enum {number}
    */
   var MessageParams = {
-    X: 120,
-    Y: 40,
-    INDENT: 20
+    MAX_WIDTH: 200,
+    LINE_HEIGHT: 20,
+    MARGIN_LEFT: 150,
+    MARGIN_TOP: 30
+    //X: 120,
+    //Y: 40,
+    //INDENT: 20
   };
   /**
    * Параметры гистограммы
-   * @const
-   * @type {number}
+   * @readonly
+   * @enum {number}
    */
   var HistogramParams = {
     HEIGHT: 150,
     INDENT: 50,
     X: 120,
     Y: 90,
-    MAX: -1,
     BAR_WIDTH: 40,
-    LINE_HEIGHT: 20
+    LINE_HEIGHT: 20,
+    MAX: -1
   };
   /**
    * Отрисовка тени облака результатов.
@@ -52,22 +56,50 @@ window.renderStatistics = function (ctx, names, times) {
   /**
    * Отрисовка сообщений.
    */
+  function wrapText(ctx, text, marginLeft, marginTop, maxWidth, lineHeight) {
+    var words = text.split(' ');
+    var countWords = words.length;
+    var line = '';
+    for (var n = 0; n < countWords; n++) {
+      var testLine = line + words[n] + ' ';
+      var testWidth = ctx.measureText(testLine).width;
+      if (testWidth > maxWidth) {
+        ctx.fillText(line, marginLeft, marginTop);
+        line = words[n] + ' ';
+        marginTop += lineHeight;
+      } else {
+        line = testLine;
+      }
+    }
+    ctx.fillText(line, marginLeft, marginTop);
+  }
+
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-
-  ctx.fillText('Ура вы победили!', MessageParams.X, MessageParams.Y);
-  ctx.fillText('Список результатов:', MessageParams.X, MessageParams.Y + MessageParams.INDENT);
+  var message = 'Ура вы победили! Список результатов:';
+  wrapText(ctx, message, MessageParams.MARGIN_LEFT, MessageParams.MARGIN_TOP, MessageParams.MAX_WIDTH, MessageParams.LINE_HEIGHT);
+  //ctx.fillText('Ура вы победили! Список результатов:', MessageParams.X, MessageParams.Y);
+  //ctx.fillText('Список результатов:', MessageParams.X, MessageParams.Y + MessageParams.INDENT);
   /**
    * Отрисовка гистограммы результатов участников.
    */
+
+  function drawRect(ctx, time, columnName, index) {
+    ctx.fillRect(HistogramParams.X + (HistogramParams.INDENT + HistogramParams.BAR_WIDTH) * index, HistogramParams.HEIGHT + HistogramParams.Y - time * step, HistogramParams.BAR_WIDTH, time * step);
+  }
+  function randomOpacity(min, max) {
+    return Math.random() * (max - min) + min;
+  }
   HistogramParams.MAX = Math.max.apply(null, times);
   var step = HistogramParams.HEIGHT / (HistogramParams.MAX - 0);
-  for (var i = 0; i < times.length; i++) {
+
+  times.forEach(function (time, index) {
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), HistogramParams.X + (HistogramParams.INDENT + HistogramParams.BAR_WIDTH) * i, HistogramParams.HEIGHT + HistogramParams.Y - times[i] * step - HistogramParams.LINE_HEIGHT / 2);
-    ctx.fillStyle = (names[i] === 'Вы') ? 'red' : 'rgba(0, 0, 255, ' + Math.random() + ')';
-    ctx.fillRect(HistogramParams.X + (HistogramParams.INDENT + HistogramParams.BAR_WIDTH) * i, HistogramParams.HEIGHT + HistogramParams.Y - times[i] * step, HistogramParams.BAR_WIDTH, times[i] * step);
+    ctx.fillText(Math.round(time), HistogramParams.X + (HistogramParams.INDENT + HistogramParams.BAR_WIDTH) * index, HistogramParams.HEIGHT + HistogramParams.Y - time * step - HistogramParams.LINE_HEIGHT / 2);
+    ctx.fillStyle = (names[index] === 'Вы') ? 'red' : 'rgba(0, 0, 255, ' + randomOpacity(0.1, 1) + ')';
+    drawRect(ctx, time, name[index], index);
+    // ctx.fillRect(HistogramParams.X + (HistogramParams.INDENT + HistogramParams.BAR_WIDTH) * index, HistogramParams.HEIGHT + HistogramParams.Y - time * step, HistogramParams.BAR_WIDTH, time * step);
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], HistogramParams.X + (HistogramParams.INDENT + HistogramParams.BAR_WIDTH) * i, HistogramParams.Y + HistogramParams.HEIGHT + HistogramParams.LINE_HEIGHT);
-  }
+    ctx.fillText(names[index], HistogramParams.X + (HistogramParams.INDENT + HistogramParams.BAR_WIDTH) * index, HistogramParams.Y + HistogramParams.HEIGHT + HistogramParams.LINE_HEIGHT);
+  });
 };
