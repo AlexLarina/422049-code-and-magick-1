14fa39d04c3519d6acfe541e9bbaadd7a18607a8
Alexandra Larina <aa.larina.ff@gmail.com>
2017-11-26 21:12:48 +0300

    Исправлены функции, реализующие создание объекта мага и создания массива таких объектов.

--- js/setup.js
+++ js/setup.js
@@ -1,23 +1,35 @@
 'use strict';
 // переменные и enam
 var WIZARDS_NUMBER = 4;
-var wizards = [];
 var WizardParams = {
   NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
   SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
   COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
   EYE_COLORS: ['black', 'red', 'blue', 'yellow', 'green']
 };
-var fragment;
 // функции
+
+function createWizard() {
+  var wizard = {
+    name: '',
+    coatColor: '',
+    eyesColor: ''
+  };
+  var rand = Math.floor(Math.random() * WizardParams.NAMES.length);
+  wizard.name = WizardParams.NAMES[rand] + ' ' + WizardParams.SURNAMES[rand];
+  rand = Math.floor(Math.random() * WizardParams.COAT_COLORS.length);
+  wizard.coatColor = WizardParams.COAT_COLORS[rand];
+  rand = Math.floor(Math.random() * WizardParams.EYE_COLORS.length);
+  wizard.eyesColor = WizardParams.EYE_COLORS[rand];
+  return wizard;
+}
+
 function createWizardsArray(wizardsNumber) {
+  var wizards = [];
   for (var i = 0; i < wizardsNumber; i++) {
-    wizards[i] = {
-      name: '',
-      coatColor: '',
-      eyesColor: ''
-    };
-  } return wizards;
+    wizards[i] = createWizard();
+  }
+  return wizards;
 }
 
 var renderWizard = function (wizard) {
@@ -31,21 +43,12 @@ var renderWizard = function (wizard) {
 };
 
 function createFragment() {
-  fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizards.length; i++) {
-    var rand = Math.floor(Math.random() * WizardParams.NAMES.length);
-    wizards[i].name = WizardParams.NAMES[rand] + ' ' + WizardParams.SURNAMES[rand];
-
-    rand = Math.floor(Math.random() * WizardParams.COAT_COLORS.length);
-    wizards[i].coatColor = WizardParams.COAT_COLORS[rand];
-
-    rand = Math.floor(Math.random() * WizardParams.EYE_COLORS.length);
-    wizards[i].eyesColor = WizardParams.EYE_COLORS[rand];
-
-    fragment.appendChild(renderWizard(wizards[i]));
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < WIZARDS_NUMBER; i++) {
+    fragment.appendChild(renderWizard(createWizardsArray(WIZARDS_NUMBER)[i]));
   }
+  similarListElement.appendChild(fragment);
 }
-//
 
 // махинации с отрисовкой
 
@@ -55,22 +58,6 @@ userDialog.classList.remove('hidden');
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-createWizardsArray(WIZARDS_NUMBER);
 createFragment();
-
-/* var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  var rand = Math.floor(Math.random() * WizardParams.NAMES.length);
-  wizards[i].name = WizardParams.NAMES[rand] + ' ' + WizardParams.SURNAMES[rand];
-
-  rand = Math.floor(Math.random() * WizardParams.COAT_COLORS.length);
-  wizards[i].coatColor = WizardParams.COAT_COLORS[rand];
-
-  rand = Math.floor(Math.random() * WizardParams.EYE_COLORS.length);
-  wizards[i].eyesColor = WizardParams.EYE_COLORS[rand];
-
-  fragment.appendChild(renderWizard(wizards[i]));
-}*/
-similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
